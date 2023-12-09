a7131f1f28847b7ae231bb33cacc862a5033c5b1
Dario de Souza Cavalcante Costa <dario.costa@sistemapoliedro.com.br>
2023-12-08 22:00:31 -0300

    chore: menu mobile

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,7 +7,7 @@ import Nav from './Nav'
 export default function Header() {
   return (
     <header className="h-16 w-full fixed bg-blue-600 top-0 shadow-md">
-      <div className="h-full mx-auto px-8 flex justify-between items-center">
+      <div className="h-full mx-auto px-8 flex justify-between items-center max-w-5xl">
         <Link href="/">Mercado do Zé</Link>
         <Nav />
       </div>
--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -3,94 +3,55 @@ import { Menu, ShoppingCart } from 'lucide-react'
 import Link from 'next/link'
 import { useState } from 'react'
 import CartCount from './CartCount'
-import useMedia from '../utils/media'
+import { X } from 'lucide-react'
+
 
 export default function Nav() {
   const [menu, setMenu] = useState(false)
 
-  const media = useMedia('(min-width: 640px)')
+
 
   function isMenu() {
     setMenu(!menu)
   }
   return (
     <nav>
-      <Menu className='sm:hidden cursor-pointer text-white' onClick={isMenu} />
-      {menu && (
-        <ul className="absolute top-16 right-0 sm:hidden bg-blue-500 w-full p-4 text-center h-[50vh] flex flex-col items-center justify-around">
-          <li className='w-full'>
-            <Link
-              className="block py-2 px-4 hover:bg-white hover:rounded duration-300"
-              href="/"
-            >
-              Home
-            </Link>
-          </li>
-          <li className='w-full'>
-            <Link
-              className="block py-2 px-4 hover:bg-white hover:rounded duration-300"
-              href="/products"
-            >
-              Produtos
-            </Link>
-          </li>
-          <li className='w-full'>
-            <Link
-              className="block py-2 px-4 hover:bg-white hover:rounded duration-300"
-              href="#"
-            >
-              Sobre
-            </Link>
-          </li>
-          <li className='w-full'>
-            <Link
-              className="flex py-2 px-4 hover:bg-white hover:rounded duration-300 relative justify-center"
-              href="/cart"
-            >
-              <ShoppingCart />
-              <CartCount />
-            </Link>
-          </li>
-        </ul>
-      )}
-
-      {media && (
-        <ul className="flex gap-3">
-          <li className='w-full'>
-            <Link
-              className="block py-2 px-4 hover:bg-white hover:rounded duration-300"
-              href="/"
-            >
-              Home
-            </Link>
-          </li>
-          <li className='w-full'>
-            <Link
-              className="block py-2 px-4 hover:bg-white hover:rounded duration-300"
-              href="/products"
-            >
-              Produtos
-            </Link>
-          </li>
-          <li className='w-full'>
-            <Link
-              className="block py-2 px-4 hover:bg-white hover:rounded duration-300"
-              href="#"
-            >
-              Sobre
-            </Link>
-          </li>
-          <li className='w-full'>
-            <Link
-              className="flex py-2 px-4 hover:bg-white hover:rounded duration-300 relative justify-center"
-              href="/cart"
-            >
-              <ShoppingCart />
-              <CartCount />
-            </Link>
-          </li>
-        </ul>
-      )}
+      {menu ? <X className='cursor-pointer text-white relative sm:hidden' onClick={isMenu} /> : <Menu className='cursor-pointer text-white relative sm:hidden' onClick={isMenu} />}
+      <ul className={`${menu ? '' : 'max-[640px]:hidden'} flex flex-col w-[70%] bg-blue-600 rounded absolute z-50 right-0 p-4 top-14 gap-3 sm:flex-row sm:static sm:p-0`}>
+        <li >
+          <Link
+            className="block py-2 px-4 hover:bg-white hover:rounded duration-300"
+            href="/"
+          >
+            Home
+          </Link>
+        </li>
+        <li >
+          <Link
+            className="block py-2 px-4 hover:bg-white hover:rounded duration-300"
+            href="/products"
+          >
+            Produtos
+          </Link>
+        </li>
+        <li >
+          <Link
+            className="block py-2 px-4 hover:bg-white hover:rounded duration-300"
+            href="#"
+          >
+            Sobre
+          </Link>
+        </li>
+        <li >
+          <Link
+            className="flex py-2 px-4 hover:bg-white hover:rounded duration-300 relative justify-center"
+            href="/cart"
+          >
+            <ShoppingCart />
+            <CartCount />
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 }
