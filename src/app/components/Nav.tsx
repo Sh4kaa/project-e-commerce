f056302986744f0be1b9e3250e88bc1febb524c4
Dario de souza <darioo_dario@hotmail.com>
2024-01-23 09:14:08 -0300

    style: textp branco na barra de navegação

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -21,7 +21,7 @@ export default function Nav() {
           :
           <Menu className='cursor-pointer text-white relative sm:hidden' onClick={isMenu} />
       }
-      <ul className={`${menu ? 'opacity-100 duration-700' : 'max-[640px]:opacity-0 right-[-100%] duration-700'} max-[640px]:h-[calc(70vh_-_4rem)] max-[640px]:rounded flex flex-col w-[70%] bg-slate-700 absolute top-14 z-50 right-0 p-4 gap-3 sm:flex-row sm:static sm:p-0`}>
+      <ul className={`${menu ? 'opacity-100 duration-700' : 'max-[640px]:opacity-0 right-[-100%] duration-700'} max-[640px]:h-[calc(70vh_-_4rem)] max-[640px]:rounded flex flex-col w-[70%] bg-slate-700 absolute top-14 z-50 right-0 p-4 gap-3 sm:flex-row sm:static sm:p-0 text-white`}>
         <li >
           <ActiveLink href='/'>
             Home
