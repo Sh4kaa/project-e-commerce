0bf67464ad68df39659721a8971893fc69b2c794
Dario de souza <darioo_dario@hotmail.com>
2024-01-25 21:16:04 -0300

    feat: balão toast de adicionar ao carrinho

--- src/app/components/ProductDetails.tsx
+++ src/app/components/ProductDetails.tsx
@@ -1,43 +1,46 @@
-"use client"
-import { useProductContext } from '@/contexts/CartContext'
-import { TypeProducts } from '@/types/prods'
-import Image from 'next/image'
-import Link from 'next/link'
-import { converterBRL } from '../utils/currencyConverter'
-import { useEffect, useState } from 'react'
-import { addToLocalStorage, getFromLocalStorage } from '../utils/localStorage'
+"use client";
+import { useProductContext } from "@/contexts/CartContext";
+import { TypeProducts } from "@/types/prods";
+import Image from "next/image";
+import Link from "next/link";
+import { converterBRL } from "../utils/currencyConverter";
+import { useEffect, useState } from "react";
+import { addToLocalStorage, getFromLocalStorage } from "../utils/localStorage";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function ProductDetails({ product }: { product: TypeProducts }) {
-  const [productInCart, setProductInCart] = useState(false)
-  const { addToCart } = useProductContext()
+  const [productInCart, setProductInCart] = useState(false);
+  const { addToCart } = useProductContext();
 
   useEffect(() => {
-    const cartItems = getFromLocalStorage('cart');
-    const isInCart = cartItems.some(item => item.id === product.id);
+    const cartItems = getFromLocalStorage("cart");
+    const isInCart = cartItems.some((item) => item.id === product.id);
     setProductInCart(isInCart);
   }, [product.id]);
 
   function sale(product: TypeProducts) {
-    const newProduct = { ...product, quantity: 1, addedToCart: true }
-    const productCart = addToCart(newProduct)
-    setProductInCart(true)
+    toast("Item adicionado");
+    const newProduct = { ...product, quantity: 1, addedToCart: true };
+    const productCart = addToCart(newProduct);
+    setProductInCart(true);
     if (productCart) {
     } else {
-      const item = getFromLocalStorage('cart')
-      item.push(newProduct)
-      addToLocalStorage(item)
+      const item = getFromLocalStorage("cart");
+      item.push(newProduct);
+      addToLocalStorage(item);
     }
   }
   return (
-    <section className='h-[calc(100vh_-_4rem)] flex items-center max-[500px]:h-auto pb-9'>
+    <section className="h-[calc(100vh_-_4rem)] flex items-center max-[500px]:h-auto pb-9">
       <div className="bg-slate-700 p-4 rounded-md max-w-3xl mx-auto">
-        <div className='flex gap-6 max-[600px]:flex-col items-center justify-center'>
+        <div className="flex gap-6 max-[600px]:flex-col items-center justify-center">
           <Image
             src={product.image}
             width={400}
             height={400}
             alt={product.category}
-            className='rounded min-w-[256px] w-full'
+            className="rounded min-w-[256px] w-full"
           />
           <div className="flex flex-col">
             <h1 className="text-center font-bold">{product.title}</h1>
@@ -46,15 +49,21 @@ export default function ProductDetails({ product }: { product: TypeProducts }) {
               {converterBRL(product.price)}
             </p>
             {productInCart ? (
-              <button className="py-2 px-4 rounded bg-red-500/50 text-white cursor-not-allowed
+              <button
+                className="py-2 px-4 rounded bg-red-500/50 text-white cursor-not-allowed
             mt-4 font-semibold text-lg active:scale-105 active:bg-red-500/70 active:text-white
-            duration-500" onClick={() => sale(product)}>
+            duration-500"
+                onClick={() => sale(product)}
+              >
                 Item Adicionado
               </button>
             ) : (
-              <button className="py-2 px-4 rounded bg-red-500
+              <button
+                className="py-2 px-4 rounded bg-red-500
               mt-4 font-semibold text-lg active:scale-105 active:bg-red-500/70 active:text-white
-              duration-500" onClick={() => sale(product)}>
+              duration-500"
+                onClick={() => sale(product)}
+              >
                 Comprar
               </button>
             )}
@@ -67,6 +76,7 @@ export default function ProductDetails({ product }: { product: TypeProducts }) {
           Continuar comprando
         </Link>
       </div>
+      <ToastContainer />
     </section>
-  )
+  );
 }
