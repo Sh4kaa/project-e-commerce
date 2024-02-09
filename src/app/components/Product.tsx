2ff29e7cb25933ee67428635c202bd5978abea0d
Dario de souza <darioo_dario@hotmail.com>
2024-02-08 21:39:57 -0300

    style: responsividade mobile e ajuste ícones

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
+import { FaCartArrowDown, FaCartPlus } from "react-icons/fa";
 import Link from "next/link";
 import Image from "next/image";
 import { TypeProducts } from "@/types/prods";
@@ -56,15 +57,14 @@ export default function Product({ children }: Props) {
       />
       <ul className="flex flex-wrap justify-center gap-4">
         {filteredListProducts.map((prod) => (
-          <li key={Math.random()} className="grow shrink basis-44 md:grow-0">
-
-            <div className="rounded-md bg-white">
-              <div className="h-28 rounded-t-md border-b-4 border-red-500 p-2 flex items-center justify-center">
+          <li key={Math.random()} className="grow shrink basis-[150px] md:grow-0">
+            <div className="rounded-md bg-white w-full">
+              <div className="flex justify-center h-[80px] py-2">
                 <Image
                   src={prod.image}
                   alt={prod.category}
+                  height={30}
                   width={60}
-                  height={60}
                 />
               </div>
               <div className="flex flex-col justify-between rounded-b bg-slate-700 text-white px-2 pb-2">
@@ -72,21 +72,21 @@ export default function Product({ children }: Props) {
                   {prod.title}
                 </h1>
                 <p className="flex items-center justify-center gap-2">
-                  <span className="text-3xl font-bold">
+                  <span className="text-2xl font-bold block">
                     {converterBRL(prod.price)}
                   </span>
                 </p>
-                <div className="flex gap-4 mt-1">
-                  <Link className="block w-full bg-red-600 rounded px-1" href={`/products/${prod.id.toString()}`}>
-                    Detalhes
+                <div className="flex gap-4 mt-1 justify-between items-end">
+                  <Link className="block px-1 text-xs hover:text-red-600" href={`/products/${prod.id.toString()}`}>
+                    Saiba mais
                   </Link>
                   {prod.addedToCart ? (
-                    <button className="block w-ful bg-red-600 rounded px-1">
-                      Added
+                    <button className="block bg-white rounded pl-1 pr-2 text-[22px] py-1 text-green-500">
+                      <FaCartArrowDown />
                     </button>
                   ) : (
-                    <button className="block w-ful bg-red-600 rounded px-1" onClick={() => handleAddToCart(prod)}>
-                      Comprar
+                    <button className="block bg-red-600 hover:bg-green-500 rounded duration-500 pl-1 pr-2 text-[22px] py-1" onClick={() => handleAddToCart(prod)}>
+                      <FaCartPlus />
                     </button>
                   )}
                 </div>
